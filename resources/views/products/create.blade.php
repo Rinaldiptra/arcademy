23c20d084e023c67c48f7e1c6fa8e40b363b2e97
Rinaldi Putra <57378133+rinaldi08@users.noreply.github.com>
2020-07-02 20:30:11 +0700

    upload

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,60 @@
+@extends('products.layout')
+  
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Add New product</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+   
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+   
+<form action="{{ route('products.store') }}" method="POST">
+    @csrf
+  
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nama Produk</strong>
+                <input class="form-control"name="nama_produk" placeholder="Nama Produk">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Keterangan</strong>
+                <input class="form-control"name="keterangan" placeholder="Keterangan">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Harga</strong>
+                <input class="form-control"name="harga" placeholder="Harga">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Jumlah</strong>
+                <input class="form-control"name="jumlah" placeholder="Jumlah">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Buat</button>
+        </div>
+    </div>
+   
+</form>
+@endsection
---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,65 @@
+@extends('products.layout')
+   
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit product</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+  
+    <form action="{{ route('products.update',$product->id) }}" method="POST">
+    
+        @csrf
+        @method('PUT')
+   
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Nama Produk:</strong>
+                    <input type="text" name="nama_produk" value="{{ $product->nama_produk }}" class="form-control" placeholder="Nama Produk">
+                </div>
+            </div>
+            <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Keterangan:</strong>
+                    <input type="text" name="keterangan" value="{{ $product->keterangan }}" class="form-control" placeholder="Keterangan">
+                </div>
+            </div>
+            <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Harga:</strong>
+                    <input type="text" name="harga" value="{{ $product->harga }}" class="form-control" placeholder="Harga">
+                </div>
+            </div>
+            <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Jumlah:</strong>
+                    <input type="text" name="jumlah" value="{{ $product->jumlah }}" class="form-control" placeholder="Jumlah">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+   
+    </form>
+@endsection
